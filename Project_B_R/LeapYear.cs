8f65a268b305a556832f22e190885e3116c9eadc
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-04-25 14:56:45 +0530

    [This is created class which name is LeapYear]

---
+++ Project_B_R/LeapYear.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    public class LeapYear
+    {
+        public static void CheckLeapYear()
+        {
+            
+           Console.WriteLine("Enter your Valid Year");
+            
+           int Year = Convert.ToInt32(Console.ReadLine());
+           
+           if ((Year > 999) && (Year<10000))
+            {
+
+                if ((Year % 4) == 0 && (Year % 100) == 0)
+                    Console.WriteLine("is a leap year.{0}", Year);
+                else if ((Year % 100) == 0)
+                    Console.WriteLine(" is not a leap year.{0}", Year);
+                else if ((Year % 4) == 0)
+                    Console.WriteLine(" is a leap year.{0}", Year);
+                else
+                    Console.WriteLine(" is not a leap year.{0}", Year);
+            }
+           else 
+            {
+                Console.WriteLine("Year is not valid");
+            }
+        }
+    }
+    
+}
