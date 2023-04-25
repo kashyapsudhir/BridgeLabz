057d266d86a6dc7416c9b9b45fcb599b88ccbfbf
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-04-25 14:51:07 +0530

    [This is main program file]

---
+++ Project_B_R/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            //FlipCoin.DisplayPerecentage();
+            //LeapYear.CheckLeapYear();
+            Power.CheckPower();
+        }
+    }
+}
